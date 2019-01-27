0d7b85de08e878bb5428fd8abbdac78dbefd76aa
Jerry Liu <jerryl1996@gmail.com>
2019-01-27 01:49:59 -0800

    Update blink_led.py

--- blink_led.py
+++ blink_led.py
@@ -4,8 +4,8 @@ import time
 GP = GPIOProcessor()
 
 try:
-    Pin27 = GP.getPin27()
-    Pin27.out()
+    Pin34 = GP.getPin34()
+    Pin34.out()
 #
 #   Pin29 = GP.getPin29()
 #   Pin29.input()	
@@ -15,10 +15,12 @@ try:
 	
 #	if pinValue == 1:
     while True:
-        Pin27.high()
+        Pin34.high()
+        print("beep")
 #       else:
         time.sleep(1)
-#       Pin27.low()
+        Pin34.low()
+        print("boop")
 #       time.sleep(1)
 
 finally:
