51e64c0a24ae4dac5b8d07ac059a51887baa7552
Jerry Liu <jerryl1996@gmail.com>
2019-01-27 01:31:24 -0800

    changed gpio pin, make LED blink

--- touch_blink.py
+++ touch_blink.py
@@ -1,13 +1,11 @@
 import mraa
+import time
 print (mraa.getVersion())
-led = mraa.Gpio(23)
+led = mraa.Gpio(12)
 led.dir(mraa.DIR_OUT)
 led.write(0)
-touch = mraa.Gpio(29)
-touch.dir(mraa.DIR_IN)
 while True:
-    touchButton = int(touch.read())
-    if(touchButton == 1):
-        led.write(1)
-    else:
-        led.write(0)
+    time.sleep(1)
+    led.write(1)
+    time.sleep(1)
+    led.write(0)
